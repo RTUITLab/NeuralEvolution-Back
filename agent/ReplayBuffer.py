e3a53331484f2775f32624f30366a5a765f944b6
temchik00 <temchik00@yandex.ru>
2020-10-01 00:17:37 +0300

    Minor changes in ReplayBuffer - Changed data type for rewards - max_length is now not a static field

--- agent/ReplayBuffer.py
+++ agent/ReplayBuffer.py
@@ -2,16 +2,15 @@ import numpy as np
 
 
 class ReplayBuffer:
-    max_length = 0
 
     def __init__(self, length, environment_shape):
-        ReplayBuffer.max_length = length
         self.pointer = 0
         self.real_length = 0
+        self.max_length = length
         self.current_states = np.empty((length, *environment_shape), dtype=np.float32)
         self.future_states = np.empty((length, *environment_shape), dtype=np.float32)
         self.actions = np.empty(length, dtype=np.int32)
-        self.rewards = np.empty(length, dtype=np.int32)
+        self.rewards = np.empty(length, dtype=np.float32)
         self.done_flags = np.empty(length, dtype=np.bool)
         pass
 
@@ -21,8 +20,8 @@ class ReplayBuffer:
         self.actions[self.pointer] = action
         self.rewards[self.pointer] = reward
         self.done_flags[self.pointer] = done_flag
-        self.real_length += int(self.real_length < ReplayBuffer.max_length)
-        self.pointer = (self.pointer + 1) % ReplayBuffer.max_length
+        self.real_length += int(self.real_length < self.max_length)
+        self.pointer = (self.pointer + 1) % self.max_length
 
     def get_experience(self, batch_size):
         batch = np.random.choice(self.real_length, batch_size, replace=False)
